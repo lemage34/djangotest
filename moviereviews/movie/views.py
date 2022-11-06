5a562bb5db0660e9f2485d223e18430b525f85d0
Vincent Artz <artzvincent@gmail.com>
2022-11-06 23:48:06 +0100

    Initial commit

---
+++ moviereviews/movie/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return render(request, 'home.html')
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
+
+# Create your views here.
